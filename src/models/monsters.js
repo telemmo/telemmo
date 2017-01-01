c0c644cbdae494f875dba157a56fc38f282c94e9
Marco Worms <marcogworms@gmail.com>
2017-01-01 15:36:12 -0200

    Rename Green Fields

--- src/models/monsters.js
+++ src/models/monsters.js
@@ -1,40 +1,135 @@
 const monsters = [
   {
-    name: 'Jelly',
+    name: 'Rat',
     maps: [
       {
-        name: 'Easy Fields',
-        spawnInfluence: 30
+        name: 'Green Fields',
+        spawnInfluence: 30,
       }
     ],
-    str: 1,
+    str: 2,
     vit: 1,
-    agi: 1,
-    luk: 3,
-    int: 3,
-    dex: 3,
+    agi: 2,
+    luk: 1,
+    int: 2,
+    dex: 1,
     loot: {
-      types: ['Rock', 'Earth'],
+      types: ['Earth'],
       max: 2,
     },
   },
   {
-    name: 'Jello',
+    name: 'Big Rat',
     maps: [
       {
-        name: 'Easy Fields',
-        spawnInfluence: 30
+        name: 'Green Fields',
+        spawnInfluence: 20,
+      }
+    ],
+    str: 4,
+    vit: 3,
+    agi: 4,
+    luk: 3,
+    int: 1,
+    dex: 3,
+    loot: {
+      types: ['Earth', 'Rock'],
+      max: 2,
+    },
+  },
+  {
+    name: 'Spider',
+    maps: [
+      {
+        name: 'Green Fields',
+        spawnInfluence: 15,
+      }
+    ],
+    str: 5,
+    vit: 5,
+    agi: 5,
+    luk: 5,
+    int: 5,
+    dex: 5,
+    loot: {
+      types: ['Wind'],
+      max: 4,
+    },
+  },
+  {
+    name: 'Snake',
+    maps: [
+      {
+        name: 'Green Fields',
+        spawnInfluence: 15,
       }
     ],
     str: 3,
-    vit: 3,
-    agi: 3,
+    vit: 2,
+    agi: 4,
+    luk: 3,
+    int: 1,
+    dex: 5,
+    loot: {
+      types: ['Wind', 'Earth'],
+      max: 2,
+    },
+  },
+  {
+    name: 'Toad',
+    maps: [
+      {
+        name: 'Green Fields',
+        spawnInfluence: 7,
+      }
+    ],
+    str: 7,
+    vit: 7,
+    agi: 5,
+    luk: 6,
+    int: 1,
+    dex: 5,
+    loot: {
+      types: ['Water',],
+      max: 5,
+    },
+  },
+  {
+    name: 'Buffalo',
+    maps: [
+      {
+        name: 'Green Fields',
+        spawnInfluence: 5,
+      }
+    ],
+    str: 10,
+    vit: 10,
+    agi: 7,
     luk: 1,
     int: 1,
-    dex: 1,
+    dex: 8,
     loot: {
-      types: ['Water', 'Ice'],
-      max: 2,
+      types: ['Rock', 'Earth', 'Wind'],
+      max: 6,
+    },
+  },
+  {
+    name: 'Eletric Buffalo',
+    maps: [
+      {
+        name: 'Green Fields',
+        spawnInfluence: 2,
+      }
+    ],
+    str: 15,
+    vit: 15,
+    agi: 10,
+    luk: 1,
+    int: 1,
+    dex: 10,
+    loot: {
+      types: ['Rock', 'Earth', 'Wind', 'Shock'],
+      max: 8,
     },
   },
 ]
--- src/views/keyboards/maps.js
+++ src/views/keyboards/maps.js
@@ -3,8 +3,8 @@ const emoji = require('node-emoji')
 module.exports = {
   reply_markup: {
     keyboard: [
-      [':boom: Easy Fields', ':no_entry_sign: Wild Place'],
-      [':no_entry_sign: Generic Desert', ':no_entry_sign: Old Town'],
+      [':boom: Green Fields', ':no_entry_sign: Cave'],
+      [':no_entry_sign: Old Factory', ':no_entry_sign: Spooky Lands'],
       ['Back to overworld :globe_with_meridians:'],
     ].map(row => row.map(el => emoji.emojify(el)))
   }
