6e1a277e064f930b927abd38d79dfef427056c28
Marco Worms <marcogworms@gmail.com>
2017-01-02 22:23:25 -0200

    add dev notes

--- src/views/overworld.js
+++ src/views/overworld.js
@@ -2,9 +2,13 @@ const emoji = require('node-emoji')
 const keyboard = require('./keyboards/overworld')
 
 module.exports = {
-  keyboard,
+  keyboard: Object.assign({}, keyboard, { parse_mode: 'markdown' }),
   message: emoji.emojify(`
 :globe_with_meridians: Welcome to the overworld! :globe_with_meridians:
+
+\`Dev notes:\`
+:no_entry_sign: = Not implemented yet :(
+
   `.trim()),
   error: `You don't have a character, create one at /start`
 }
