7062aae32911ce0c8ac89548bcd4f95db0147809
Marco Worms <marcogworms@gmail.com>
2017-01-09 22:53:25 -0200

    Rebalance stats

--- src/models/combat.js
+++ src/models/combat.js
@@ -24,17 +24,17 @@ function applyBuff (buffs, stats) {
 
 function getStats (fighter) {
   return {
-    maxHp: 30 + Math.floor(fighter.vit/2),
-    hp:  30 + Math.floor(fighter.vit/2),
+    maxHp: 50 + Math.floor(fighter.vit/1.5),
+    hp:  50 + Math.floor(fighter.vit/1.5),
     aspd: fighter.agi * 2 + 50,
     atk: 10 + fighter.str + Math.floor(fighter.dex/2),
-    def: 5 + Math.floor(fighter.vit/3),
+    def: 5 + Math.floor(fighter.vit/2),
     atkVariation: 0.3 - fighter.dex/350,
     skillCast: 0.1 + fighter.dex/250,
     stunChance: 0,
     dropRatio: 1 + fighter.luk/250,
     dodge: Math.min(0.03 + fighter.agi/3000, 0.8),
-    critChance: fighter.luk/150,
-    critDmg: 2 + fighter.str/100 + fighter.luk/200,
+    critChance: Math.min(fighter.luk/400, 0.9),
+    critDmg: 2 + fighter.str/200 + fighter.luk/200,
   }
 }
