6aabf2b35914b4684ef98f8001ee3aefd31ecc0e
Derek Stavis <dekestavis@gmail.com>
2017-01-21 21:44:08 -0200

    dice: refactor random with promises

--- src/game/core/dice.js
+++ src/game/core/dice.js
@@ -1,51 +1,52 @@
 import {
-  repeat,
+  flip,
+  add,
+  divide,
+  invoker,
+  times,
   zipObj,
   partial,
-  map,
 } from 'ramda'
 
 import crypto from 'crypto'
+import Promise from 'bluebird'
 
-export function d (faces) {
-  return new Promise((resolve, reject) => {
-    if (faces > 256) { reject('No more than 256 faces') }
-    crypto.randomBytes(1, (err, buf) => {
-      if (err) {
-        reject()
-      }
-      resolve(
-        Math.floor((buf[0]) / ((256) / faces)) + 1,
-      )
-    })
-  })
+const randomBytes = Promise.promisify(crypto.randomBytes)
+
+export function roll (faces) {
+  return randomBytes(1)
+    .then(invoker(0, 'readUInt8'))
+    .then(flip(divide)(256 / faces))
+    .then(add(1))
+    .then(Math.floor)
 }
 
 export function rollBatch (faces, meanings) {
-  const rolls = repeat(partial(d, [faces]), meanings.length)
-  return Promise
-    .all(rolls.map(p => p()))
-    .then(partial(zipObj, [meanings]))
+  const rolls = times(partial(roll, [faces]), meanings.length)
+  return Promise.all(rolls)
+    .then(zipObj(meanings))
 }
 
-const testRolls = () => {
-  var x = 0
-  var y = 0
-  repeat(
-    () => d(20).then((r) => {
-      x += r
-      y += 1
-      console.log(x / y, 'sould aim for 10.5')
-    }),
-    100000,
-  ).map(r => r())
-}
+// const testRolls = () => {
+//   var x = 0
+//   var y = 0
+//   repeat(
+//     () => roll(20).then((r) => {
+//       x += r
+//       y += 1
+//       console.log(x / y, 'sould aim for 10.5')
+//     }),
+//     100000,
+//   ).map(r => r())
+// }
 
 export const test = () => {
   // testRolls()
-
-  // rollBatch(20, ['team 1 initiative', 'team 2 initiative'])
-  //   .then(console.log)
+  //
+  // times(() => {
+  //   rollBatch(20, ['team 1 initiative', 'team 2 initiative'])
+  //     .then(console.log)
+  // }, 100)
   //
   // Promise.all(
   //   repeat(
