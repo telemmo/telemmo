83355173ac22757381db58ef85fa8adf42ca45d9
Marco Worms <marcogworms@gmail.com>
2017-01-25 01:02:24 -0200

    core: remove console.log

--- src/game/core/randomSkillFromStance.js
+++ src/game/core/randomSkillFromStance.js
@@ -6,13 +6,10 @@ import models from '../models'
 
 export default function randomSkillFromStance (id) {
   const stance = models.stances.find(id)
-  const skills = stance.skills.reduce((acc, skill) => {
-    console.log(skill)
-    return [
-      ...acc,
-      ...Array.from({ length: skill.influence }).map(always(skill.id))
-    ]
-  }, [])
+  const skills = stance.skills.reduce((acc, skill) => [
+    ...acc,
+    ...Array.from({ length: skill.influence }).map(always(skill.id)),
+  ], [])
   const skillId = skills[Math.floor(Math.random() * skills.length)]
   const skill = models.skills.find(skillId)
   return skill
