0ae829d5eefb8dfca5c22c2a3e0d2370aa1486e5
Derek Stavis <dekestavis@gmail.com>
2017-01-28 13:14:47 -0200

    webpack: add live reload to dev task

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,11 @@
-
 const config = require('./webpack.config.js')
+const WebpackShellPlugin = require('webpack-shell-plugin')
 
 config.output.filename = 'telemmo.dev.js'
+config.plugins.push(new WebpackShellPlugin({
+  onBuildExit: [
+    'node dist/telemmo.dev.js',
+  ],
+}))
 
 module.exports = config
