99079f4097a48b375a4f631ab37fa609cfb754b4
Marco Worms <marcogworms@gmail.com>
2017-01-28 18:45:00 -0200

    models: better tests

--- src/game/models/models.test.js
+++ src/game/models/models.test.js
@@ -6,7 +6,14 @@ function findAll(needle, haysack) {
       models[needle].find(e.id)))
 }
 
+function findAllPlain(needle, haysack) {
+  models[haysack].all.map(hay =>
+    hay[needle].map(e =>
+      models[needle].find(e)))
+}
+
 test('models', () => {
   findAll('monsters', 'maps')
   findAll('skills', 'stances')
+  findAllPlain('stances', 'classes')
 })
