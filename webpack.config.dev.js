1b6a5b4716ccdca96f51e61d3534c84ab70d0642
Derek Stavis <dekestavis@gmail.com>
2017-01-29 22:16:21 -0200

    chore: remove live reload

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,5 @@
 const config = require('./webpack.config.js')
-const WebpackShellPlugin = require('webpack-shell-plugin')
 
 config.output.filename = 'telemmo.dev.js'
-config.plugins.push(new WebpackShellPlugin({
-  onBuildExit: [
-    'node dist/telemmo.dev.js',
-  ],
-}))
 
 module.exports = config
