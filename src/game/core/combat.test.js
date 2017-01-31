e4eba0d1391fb40bab289272362d2bb41e141be4
Marco Worms <marcogworms@gmail.com>
2017-01-31 00:32:57 -0200

    combat: ignore broken tests

--- src/game/core/combat.test.js
+++ src/game/core/combat.test.js
@@ -35,12 +35,6 @@ function runCombat (teams) {
     })
 }
 
-// test('combat', () => {
-//   const teams = (exploreMock(100, 'arcane', 'death'))
-//   return runCombat(teams)
-//     // .tap(c => console.log(JSON.stringify(c, null, 2)))
-// });
-
 function runCombatBatch (teams, amount, log) {
   return Promise.all(times(partial(runCombat, [teams]), amount))
     .tap((combats) => {
@@ -51,33 +45,15 @@ function runCombatBatch (teams, amount, log) {
     .tap(console.log.bind(console, log))
 }
 
-// test('combatBalance', () => {
-//   return Promise.all([
-//     // runCombatBatch(exploreMock(100, 'arcane', 'death'), 100, 'arcane'),
-//     // runCombatBatch(exploreMock(100, 'debuff', 'death'), 100, 'debuff'),
-//     // runCombatBatch(exploreMock(100, 'endure', 'death'), 100, 'endure'),
-//     // runCombatBatch(exploreMock(100, 'berserk', 'death'), 100, 'berserk'),
-//     // runCombatBatch(exploreMock(100, 'poison', 'death'), 100, 'poison'),
-//     // runCombatBatch(exploreMock(100, 'stealth', 'death'), 100, 'stealth'),
-//     // runCombatBatch(exploreMock(100, 'martial', 'death'), 100, 'martial'),
-//     // runCombatBatch(exploreMock(100, 'buffer', 'death'), 100, 'buffer'),
-//     // runCombatBatch(exploreMock(100, 'sniper', 'death'), 100, 'sniper'),
-//     // runCombatBatch(exploreMock(100, 'trapper', 'death'), 100, 'trapper'),
-//     // runCombatBatch(exploreMock(100, 'support', 'death'), 100, 'support'),
-//     // runCombatBatch(exploreMock(100, 'breaker', 'death'), 100, 'breaker'),
-//     // runCombatBatch(exploreMock(5, 'arcane', 'snake'), 100, 'arcane'),
-//     // runCombatBatch(exploreMock(5, 'debuff', 'snake'), 100, 'debuff'),
-//     // runCombatBatch(exploreMock(5, 'endure', 'snake'), 100, 'endure'),
-//     // runCombatBatch(exploreMock(5, 'berserk', 'snake'), 100, 'berserk'),
-//     // runCombatBatch(exploreMock(5, 'poison', 'snake'), 100, 'poison'),
-//     // runCombatBatch(exploreMock(5, 'stealth', 'snake'), 100, 'stealth'),
-//     // runCombatBatch(exploreMock(5, 'martial', 'snake'), 100, 'martial'),
-//     // runCombatBatch(exploreMock(5, 'buffer', 'snake'), 100, 'buffer'),
-//     // runCombatBatch(exploreMock(5, 'sniper', 'snake'), 100, 'sniper'),
-//     // runCombatBatch(exploreMock(5, 'trapper', 'snake'), 100, 'trapper'),
-//     // runCombatBatch(exploreMock(5, 'support', 'snake'), 100, 'support'),
-//     // runCombatBatch(exploreMock(5, 'breaker', 'snake'), 100, 'breaker'),
-//   ])
-// })
+test.skip('combat', () => {
+  const teams = (exploreMock(100, 'arcane', 'death'))
+  return runCombat(teams)
+    // .tap(c => console.log(JSON.stringify(c, null, 2)))
+})
 
+test.skip('combatBalance', () => {
+  return Promise.all([
+    // runCombatBatch(exploreMock(5, 'breaker', 'snake'), 100, 'breaker'),
+  ])
+})
 
