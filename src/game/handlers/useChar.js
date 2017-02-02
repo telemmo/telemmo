da9b7b595578a6be7c458a3d01d736b161a0e128
Marco Worms <marcogworms@gmail.com>
2017-02-02 15:18:15 -0200

    handlers: fix bad input validation at /use_char route

--- src/game/handlers/useChar.js
+++ src/game/handlers/useChar.js
@@ -7,7 +7,7 @@ import {
 
 import { ObjectId } from 'mongodb'
 
-import { rejectUndefined } from './errors'
+import { reject, rejectUndefined } from './errors'
 
 function useChar (dao, playerId, char) {
   return dao.player.update(
@@ -19,13 +19,17 @@ function useChar (dao, playerId, char) {
 export default function call (dao, provider, _, msg) {
   const charId = msg.matches[1]
 
+  if (!ObjectId.isValid(charId)) {
+    return reject(msg, _('Character ID is not a valid key ID'))
+  }
+
   return dao.character
     .find({
       _id: ObjectId(charId),
       playerId: msg.player.id,
     })
     .then(nth(0))
-    .then(rejectUndefined(msg, _('Invalid char Id')))
+    .then(rejectUndefined(msg, _('This character for this player was not found.')))
     .then(partial(useChar, [dao, msg.player.id]))
     .then(char => ({
       to: msg.chat,
