a70b31a7f6e5bcd799e18eff1ca01faa7624f77a
klaygomes <klaygomes@gmail.com>
2017-02-03 01:05:36 -0200

    core: tests for explore.js updated

--- src/game/core/explore.test.js
+++ src/game/core/explore.test.js
@@ -1,5 +1,28 @@
 test('get random monster from map', () => {
   const { randomMonster } = require('./explore')
-  const monster = randomMonster('green_fields')
-  expect(monster.prizes).toBeTruthy();
+
+    //given some monsters
+    let monsters =  Array.from({length:10}, function(){
+            return {
+                id:'monster_' + Math.random(),
+                influence: Math.floor(Math.random()*10)
+            }
+        }
+    )
+
+    //we must pick one random
+  const monster = randomMonster(
+      monsters,
+      function stubFind(id){
+        return {
+          id
+        }
+      }
+  )
+
+  //not null or undefined
+  expect(monster).toBeDefined()
+
+  //it must be at least one of the monsters given
+  expect(monsters.find( mtr=> mtr.id === monster.id)).toBeTruthy()
 });
