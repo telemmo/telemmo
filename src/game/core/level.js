916d12b365c276666aecaba7f5bb7b8240d53a2a
muvlon <muvlon@hentzes.de>
2017-02-08 02:33:44 +0100

    change [ and ] to | in exp bar

--- src/game/core/level.js
+++ src/game/core/level.js
@@ -36,18 +36,17 @@ export function buildExpBar (percentage, size = 10) {
   const nFilled = Math.floor(percentage * size)
   const nEmpty = size - nFilled - 1
   const smallBars = Array.from(' ▏▎▍▌▋▊▉')
-  const begin = '['
-  const end = ']'
+  const delimiter = '|'
   return [
     `${(percentage * 100).toFixed(2)}%`,
     [
-      begin,
+      delimiter,
       reduce(concat, [], [
         Array.from({ length: nFilled }, always('█')),
         [smallBars[Math.floor((percentage*size - nFilled)*8)]],
         Array.from({ length: nEmpty },  always(' ')),
       ]).join(''),
-      end,
+      delimiter,
     ].join(''),
   ].join(' ')
 }
