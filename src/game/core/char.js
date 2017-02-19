e0238fb6da83c70fb8bdd195aca4f860ea3ecebb
Derek Stavis <dekestavis@gmail.com>
2017-02-18 22:14:39 -0200

    explore: allow change char while in combat

--- src/game/core/char.js
+++ src/game/core/char.js
@@ -15,11 +15,6 @@ export function useChar (dao, playerId, char) {
       'teams.members.id': { $in: map(prop('id'), chars) },
       finishedAt: { $exists: false },
     }))
-    .then(ifElse(
-      pipe(length, equals(0)),
-      always(undefined),
-      () => Promise.reject(new Error('A combat is pending')),
-    ))
     .then(partial(dao.player.update, [
       { _id: playerId },
       { $set: { currentCharId: char.id } },
