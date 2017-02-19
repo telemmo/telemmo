602dae7287e4804dc34cecd72105c34acc1a1553
Marco Worms <marcogworms@gmail.com>
2017-02-19 17:48:51 -0300

    combat: move utils to new file

---
+++ src/game/core/combat/addTeamsLevels.js
@@ -0,0 +1,34 @@
+import {
+  find,
+  partial,
+  propEq,
+  propOr,
+  pipe,
+  merge,
+} from 'ramda'
+
+import { level } from '../level'
+import { teamsMemberIds } from './utils'
+
+function mergeLevel (teams, computedExps) {
+  return teams.map(team =>
+    team.map((char) => {
+      if (char.prizes) { return char }
+      const charExp = pipe(find(propEq('_id', char.id)), propOr(0, 'exp'))
+      const exp = charExp(computedExps)
+      return merge(char, { exp, level: level({ exp }) })
+    }))
+}
+
+export default function (dao, teams) {
+  const members = teamsMemberIds(teams)
+
+  return dao.combat.aggregate([
+    { $match: { winners: { $in: members } } },
+    { $project: { prizes: 1 } },
+    { $unwind: '$prizes' },
+    { $project: { exp: '$prizes.exp', charId: '$prizes.charId' } },
+    { $group: { _id: '$charId', exp: { $sum: '$exp' } } },
+  ])
+    .then(partial(mergeLevel, [teams]))
+}
