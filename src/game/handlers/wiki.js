f63d8c21a00f07a1b21f2b9f655789f3681eb89a
PlatypusKing <jacob.baumann@gmail.com>
2017-04-03 00:57:47 -0500

    A (hopefully) wiki command

---
+++ src/game/handlers/wiki.js
@@ -0,0 +1,20 @@
+export default function call (dao, provider, _, msg) {
+  const params = {
+    to: msg.chat,
+    text: _([
+      _(':mortar_board: TeleMMO Wiki'),
+      '',
+      _('Find up to date information on'),
+      _('  :small_blue_diamond: Classes'),
+      _('  :small_blue_diamond: Mechanics'),
+      _('  :small_blue_diamond: Maps'),
+      _('  :small_blue_diamond: Equipment'),
+      '',
+      _('and more, all at:'),
+      _('reddit.com/r/telemmo/wiki'),
+      '',
+    ]),
+  }
+
+  return Promise.resolve(params)
+}
